d7fb5ea73c4d01a5c1078f8c8ae823cd9d9be89f
Alexander <154531713+v1ntageo@users.noreply.github.com>
2023-12-22 12:29:44 +0300

    Delete Intellectra directory

--- Intellectra/News page/js/main.js
+++
@@ -1,6 +0,0 @@
-let readmore = document.querySelector('.blog__readmore');
-let blogItemsHidden = document.querySelector('.blog__items--second');
-readmore.addEventListener('click', function(){
-  blogItemsHidden.classList.remove('hidden');
-  readmore.remove();
-});
